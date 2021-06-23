8062bf5139ae93955d7126a7270a3798b7e5a4f3
owdver <56756946+owdver@users.noreply.github.com>
2021-06-23 20:44:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,5 +13,10 @@ Iam Sorry This Is Not For You 😓.
 
 
     ABOUT_MSG = """
-
+<b>○ ♻️ ᴍʏ ᴄʀᴇᴀᴛᴏʀ :</b> <b><a href='https://t.me/owdver_bot'>ᴏᴡᴅᴠᴇʀ ʙᴏᴛ</a></b>
+<b>○ ♻️ sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ :</b> <b><a href='https://t.me/OB_SERIES'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>○ ♻️ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ :</b> <b><a href='https://t.me/OB_MOVIES'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>○ ♻️ sᴇʀɪᴇs ɢʀᴏᴜᴘ :</b> <b><a href='https://t.me/OB_SERIESGROUP'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>○ ♻️ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ :</b> <b><a href='https://t.me/OB_MOVIESGROUP'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>○ ♻️ ᴍᴀɪɴ ᴄʜᴀɴᴇʟ :</b> <b><a href='https://t.me/OB_LINKS'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
 """
