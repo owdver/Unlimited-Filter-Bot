84983bc995bddd41fbfb94a79dc5b64ba4463bcd
owdver <56756946+owdver@users.noreply.github.com>
2021-06-23 20:57:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("👩‍💻 Creator", url="https://t.me/owdver_bot"),
+                    InlineKeyboardButton("ℹ️ About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("♀️Channel", url="https://t.me/ob_movies"),
+                    InlineKeyboardButton("Group♀️", url="https://t.me/ob_moviesgroup")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "Main Channel ♂️", url="https://t.me/ob_links")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
